e0269ea3a0092a44ae5406b625190ab67f04fb21
theeskhaton <theeskhaton@gmail.com>
2013-09-23 10:27:08 +0200

    Fixed infinite date loop

--- tecaj.js
+++ tecaj.js
@@ -13,12 +13,12 @@ var logger = require('winston'),
 	was successful, parse method is called.
 	Ambiguous error message is displayed when
 	something wrong happened. */
+    d = new Date(),
     callback = function (error, response, body) {
         if (error) {
             logger.log(error);
         } else if (response.statusCode == 404) {
             logger.log('error','No document found under provided date. Trying day before');
-            d = new Date();
             d.setDate(d.getDate() - 1);
             usedDate = dateString(d);
             var link = "http://www.hnb.hr/tecajn/f" + usedDate + ".dat";
