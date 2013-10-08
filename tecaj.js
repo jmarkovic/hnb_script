5bb181b3dbb01904a63097b0456f0635db72920f
Josip Marković <josip.markovic09@gmail.com>
2013-10-08 22:23:50 +0200

    added check if entry already exists in the database

--- tecaj.js
+++ tecaj.js
@@ -1,39 +1,47 @@
 var logger = require('winston'),
     request = require("request"),
-    db = require('./lib/db');
-    models = require('./lib/models')
+    db = require('./lib/db'),
+    models = require('./lib/models'),
     usedDate = '',
     d = new Date(),
 /* Returns a string representing a date in 'ddMMyy'
-	string format. */
+ string format. */
     helpers = require('./lib/helpers'),
     dateString = helpers.dateString,
     parseBody = helpers.parseBody,
 /* Callback function called on request.
-	Checks if any 'checked' error has occured,
-	and if so, displays proper message. If request
-	was successful, parse method is called.
-	Ambiguous error message is displayed when
-	something wrong happened. */
+ Checks if any 'checked' error has occurred,
+ and if so, displays proper message. If request
+ was successful, parse method is called.
+ Ambiguous error message is displayed when
+ something wrong happened. */
     callback = function (error, response, body) {
         if (error) {
             logger.log(error);
         } else if (response.statusCode == 404) {
-            logger.log('error','No document found under provided date. Trying day before');
+            logger.log('error', 'No document found under provided date. Trying day before');
             d.setDate(d.getDate() - 1);
             usedDate = dateString(d);
             var link = "http://www.hnb.hr/tecajn/f" + usedDate + ".dat";
             request(link, callback);
         } else if (!error && response.statusCode == 200) {
             logger.log('info', body);
-            logger.log('info',JSON.stringify(parseBody(body)));
+//            logger.log('info', JSON.stringify(parseBody(body)));
             db.saveIt(parseBody(body));
         } else {
-            logger.log('error',"Error has occured");
+            logger.log('error', "Error has occured");
         }
     }
 
 usedDate = dateString(d);
 var link = "http://www.hnb.hr/tecajn/f" + usedDate + ".dat";
 
-request(link, callback);
+db.findOneOf([{'key' : 'display', 'value' : '08102013'}], models.CurrencyTable, function(result) {
+    if (!result) {
+        console.log('requesting from web');
+        request(link, callback);
+    } else {
+        console.log('it exists in the database: ' + result);
+    }
+});
+
