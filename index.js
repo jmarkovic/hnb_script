3b74e02220207fac28b270dda164227f12f0c2cb
Alen <theeskhaton@gmail.com>
2013-11-04 10:25:02 +0100

    Placed all relevant code into lib folder

--- index.js
+++ index.js
@@ -1,4 +1,2 @@
-var tecaj = require('./tecaj.js');
-tecaj.getTecaj(process.argv[2], function(result) {
-  console.log('THIS IS A RESULT: ' + JSON.stringify(result));
-});
+var tecaj = require('./lib/tecaj.js');
+module.exports = tecaj;
