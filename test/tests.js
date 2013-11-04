f1ba116af567f59c4199fac89d824c526b94b391
Alen <theeskhaton@gmail.com>
2013-11-04 11:11:49 +0100

    Updated package.json to include testing method, added another test

--- test/tests.js
+++ test/tests.js
@@ -5,4 +5,10 @@ describe('tecaj', function(){
     it('should have a getTecaj method', function(){
         tecaj.should.have.property('getTecaj').with.type('function');
     });
+    it('should return a value when called', function(done){
+        tecaj.getTecaj(null, function(t){
+            should.exist(t); 
+            done();
+        });
+    });
 });
