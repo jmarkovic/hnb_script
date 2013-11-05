1232864d379b2b4cf613ba0823b90fe2de11a300
Alen <theeskhaton@gmail.com>
2013-11-05 12:11:49 +0100

    Fixed #8, Added mocha and tests

--- test/tests.js
+++ test/tests.js
@@ -5,9 +5,15 @@ describe('tecaj', function(){
     it('should have a getTecaj method', function(){
         tecaj.should.have.property('getTecaj').with.type('function');
     });
-    it('should return a value when called', function(done){
+    it('should return a value when called with 2 parameters', function(done){
         tecaj.getTecaj(null, function(t){
             should.exist(t); 
+            done();
+        });
+    });
+    it('should return a value when called with just a callback', function(done){
+        tecaj.getTecaj(function(t){
+            should.exist(t);
             done();
         });
     });
